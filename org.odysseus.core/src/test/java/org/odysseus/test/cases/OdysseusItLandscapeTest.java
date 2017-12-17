4da4c220a4c5670851a73f1f5afa603b2b2f26d5
Martin Häusler <martin.haeusler89@gmail.com>
2017-12-17 13:47:14 +0100

    Restructuring test packages and minor fixes.

--- org.odysseus.core/src/test/java/org/odysseus/test/cases/OdysseusItLandscapeTest.java
+++ org.odysseus.core/src/test/java/org/odysseus/test/cases/OdysseusItLandscapeTest.java
@@ -1,4 +1,4 @@
-package org.odysseus.test.edpl;
+package org.odysseus.test.cases;
 
 import static com.google.common.base.Preconditions.*;
 import static org.junit.Assert.*;
@@ -15,29 +15,16 @@ import org.eclipse.emf.ecore.EcorePackage;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.odysseus.api.MutatorSelector;
 import org.odysseus.api.Odysseus;
 import org.odysseus.api.RepositoryConnector;
 import org.odysseus.exceptions.OdysseusConfigurationException;
-import org.odysseus.spi.module.SocialNetworkModule;
-import org.odysseus.spi.module.mutators.AddCommentToPostMutator;
-import org.odysseus.spi.module.mutators.AddFriendMutator;
-import org.odysseus.spi.module.mutators.AddReactionToPostMutator;
-import org.odysseus.spi.module.mutators.AddUserMutator;
-import org.odysseus.spi.module.mutators.ChildBirthMutator;
-import org.odysseus.spi.module.mutators.CreateGroupMutator;
-import org.odysseus.spi.module.mutators.CreatePostMutator;
-import org.odysseus.spi.module.mutators.JoinGroupMutator;
-import org.odysseus.spi.module.mutators.MarriageMutator;
-import org.odysseus.spi.module.mutators.SharePostMutator;
-import org.odysseus.spi.module.mutators.UploadMediaFileMutator;
+import org.odysseus.spi.module.ItLandscapeModule;
 
 import com.google.common.io.Files;
 
-public class OdysseusSocialNetworkTest {
-
+public class OdysseusItLandscapeTest {
 	@Test
 	public void canInstantiateOdysseus() {
 		Odysseus odysseus = Odysseus.newInstance();
@@ -59,7 +46,7 @@ public class OdysseusSocialNetworkTest {
 	@Test
 	public void canGenerateSimpleModel() {
 		Odysseus odysseus = Odysseus.newInstance();
-		odysseus.moduleConfig().setActiveModule(SocialNetworkModule.class);
+		odysseus.moduleConfig().setActiveModule(ItLandscapeModule.class);
 		ResourceSet resourceSet = new ResourceSetImpl();
 		File tempDir = Files.createTempDir();
 		tempDir.deleteOnExit();
@@ -72,7 +59,7 @@ public class OdysseusSocialNetworkTest {
 			odysseus.repositoryConfig().setConnector(repo);
 			odysseus.simulationConfig().stopAfterIterions(50);
 			odysseus.simulationConfig().setMutatorSelector(MutatorSelector.baseLikelyhood());
-			odysseus.randomGeneratorConfig().setRandomSeed("MODELS2017");
+			odysseus.randomGeneratorConfig().setRandomSeed("SoSyM2018");
 			odysseus.runSimulation();
 			// afterwards, print the model
 			System.out.println(FileUtils.readFileToString(tempFile, StandardCharsets.UTF_8));
@@ -89,49 +76,9 @@ public class OdysseusSocialNetworkTest {
 	}
 
 	@Test
-	@Ignore("this is for manual testing")
-	public void generateLargeModel() {
-		Odysseus odysseus = Odysseus.newInstance();
-		odysseus.moduleConfig().setActiveModule(SocialNetworkModule.class);
-		ResourceSet resourceSet = new ResourceSetImpl();
-		File tempDir = Files.createTempDir();
-		tempDir.deleteOnExit();
-		try {
-			File tempFile = new File(tempDir, "test.xmi");
-			tempFile.deleteOnExit();
-			tempFile.createNewFile();
-			Resource resource = resourceSet.createResource(URI.createFileURI(tempFile.getAbsolutePath()));
-			RepositoryConnector repo = odysseus.createResourceConnector(resource, false);
-			odysseus.repositoryConfig().setConnector(repo);
-			odysseus.simulationConfig().setFlushInterval(10_000);
-			odysseus.simulationConfig().stopAfterIterions(200_000);
-			odysseus.simulationConfig().setMutatorSelector(MutatorSelector.baseLikelyhood());
-			odysseus.randomGeneratorConfig().setRandomSeed("MODELS2017");
-			odysseus.runSimulation();
-			// afterwards, print the model
-			String xmi = FileUtils.readFileToString(tempFile, StandardCharsets.UTF_8);
-			System.out.println(StringUtils.abbreviate(xmi, 3000));
-			System.out.println("TEMP DIR IS AT: " + tempDir.getAbsolutePath());
-			System.out.println("PRESS ENTER TO EXIT");
-			try (Scanner scanner = new Scanner(System.in)) {
-				scanner.nextLine();
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				FileUtils.deleteDirectory(tempDir);
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-	}
-
-	@Test
-	@Ignore("this is for manual testing")
 	public void canGenerateDenseModel() {
 		Odysseus odysseus = Odysseus.newInstance();
-		odysseus.moduleConfig().setActiveModule(SocialNetworkModule.class);
+		odysseus.moduleConfig().setActiveModule(ItLandscapeModule.class);
 		ResourceSet resourceSet = new ResourceSetImpl();
 		File tempDir = Files.createTempDir();
 		tempDir.deleteOnExit();
@@ -143,6 +90,7 @@ public class OdysseusSocialNetworkTest {
 			RepositoryConnector repo = odysseus.createResourceConnector(resource, true);
 			odysseus.repositoryConfig().setConnector(repo);
 			odysseus.simulationConfig().setFlushInterval(10_000);
+			odysseus.simulationConfig().setMutatorSelector(MutatorSelector.baseLikelyhood());
 			odysseus.simulationConfig().setStopCriterion(context -> {
 				if (context.getIteration() % 1000 != 0) {
 					return false;
@@ -151,30 +99,7 @@ public class OdysseusSocialNetworkTest {
 				System.out.println("EObjects: " + count);
 				return count >= 200_000;
 			});
-			odysseus.simulationConfig().setMutatorSelector(MutatorSelector.baseLikelyhood()
-					//
-					.likelyhood(AddCommentToPostMutator.class, 10)
-					//
-					.likelyhood(AddFriendMutator.class, 10)
-					//
-					.likelyhood(AddReactionToPostMutator.class, 20)
-					//
-					.likelyhood(AddUserMutator.class, 5)
-					//
-					.likelyhood(ChildBirthMutator.class, 1)
-					//
-					.likelyhood(CreateGroupMutator.class, 3)
-					//
-					.likelyhood(CreatePostMutator.class, 14)
-					//
-					.likelyhood(JoinGroupMutator.class, 4)
-					//
-					.likelyhood(MarriageMutator.class, 2)
-					//
-					.likelyhood(SharePostMutator.class, 8)
-					//
-					.likelyhood(UploadMediaFileMutator.class, 5));
-			odysseus.randomGeneratorConfig().setRandomSeed("MODELS2017");
+			odysseus.randomGeneratorConfig().setRandomSeed("SoSyM2018");
 			odysseus.runSimulation();
 			// afterwards, print the model
 			String xmi = FileUtils.readFileToString(tempFile, StandardCharsets.UTF_8);
@@ -194,5 +119,4 @@ public class OdysseusSocialNetworkTest {
 			}
 		}
 	}
-
 }
--- org.odysseus.core/src/test/java/org/odysseus/test/cases/OdysseusSocialNetworkTest.java
+++ org.odysseus.core/src/test/java/org/odysseus/test/cases/OdysseusSocialNetworkTest.java
@@ -1,4 +1,4 @@
-package org.odysseus.test.edpl;
+package org.odysseus.test.cases;
 
 import static com.google.common.base.Preconditions.*;
 import static org.junit.Assert.*;
@@ -21,11 +21,23 @@ import org.odysseus.api.MutatorSelector;
 import org.odysseus.api.Odysseus;
 import org.odysseus.api.RepositoryConnector;
 import org.odysseus.exceptions.OdysseusConfigurationException;
-import org.odysseus.spi.module.ItLandscapeModule;
+import org.odysseus.spi.module.SocialNetworkModule;
+import org.odysseus.spi.module.mutators.AddCommentToPostMutator;
+import org.odysseus.spi.module.mutators.AddFriendMutator;
+import org.odysseus.spi.module.mutators.AddReactionToPostMutator;
+import org.odysseus.spi.module.mutators.AddUserMutator;
+import org.odysseus.spi.module.mutators.ChildBirthMutator;
+import org.odysseus.spi.module.mutators.CreateGroupMutator;
+import org.odysseus.spi.module.mutators.CreatePostMutator;
+import org.odysseus.spi.module.mutators.JoinGroupMutator;
+import org.odysseus.spi.module.mutators.MarriageMutator;
+import org.odysseus.spi.module.mutators.SharePostMutator;
+import org.odysseus.spi.module.mutators.UploadMediaFileMutator;
 
 import com.google.common.io.Files;
 
-public class OdysseusItLandscapeTest {
+public class OdysseusSocialNetworkTest {
+
 	@Test
 	public void canInstantiateOdysseus() {
 		Odysseus odysseus = Odysseus.newInstance();
@@ -47,7 +59,7 @@ public class OdysseusItLandscapeTest {
 	@Test
 	public void canGenerateSimpleModel() {
 		Odysseus odysseus = Odysseus.newInstance();
-		odysseus.moduleConfig().setActiveModule(ItLandscapeModule.class);
+		odysseus.moduleConfig().setActiveModule(SocialNetworkModule.class);
 		ResourceSet resourceSet = new ResourceSetImpl();
 		File tempDir = Files.createTempDir();
 		tempDir.deleteOnExit();
@@ -60,7 +72,7 @@ public class OdysseusItLandscapeTest {
 			odysseus.repositoryConfig().setConnector(repo);
 			odysseus.simulationConfig().stopAfterIterions(50);
 			odysseus.simulationConfig().setMutatorSelector(MutatorSelector.baseLikelyhood());
-			odysseus.randomGeneratorConfig().setRandomSeed("SoSyM2018");
+			odysseus.randomGeneratorConfig().setRandomSeed("MODELS2017");
 			odysseus.runSimulation();
 			// afterwards, print the model
 			System.out.println(FileUtils.readFileToString(tempFile, StandardCharsets.UTF_8));
@@ -78,9 +90,9 @@ public class OdysseusItLandscapeTest {
 
 	@Test
 	@Ignore("this is for manual testing")
-	public void canGenerateDenseModel() {
+	public void generateLargeModel() {
 		Odysseus odysseus = Odysseus.newInstance();
-		odysseus.moduleConfig().setActiveModule(ItLandscapeModule.class);
+		odysseus.moduleConfig().setActiveModule(SocialNetworkModule.class);
 		ResourceSet resourceSet = new ResourceSetImpl();
 		File tempDir = Files.createTempDir();
 		tempDir.deleteOnExit();
@@ -89,19 +101,12 @@ public class OdysseusItLandscapeTest {
 			tempFile.deleteOnExit();
 			tempFile.createNewFile();
 			Resource resource = resourceSet.createResource(URI.createFileURI(tempFile.getAbsolutePath()));
-			RepositoryConnector repo = odysseus.createResourceConnector(resource, true);
+			RepositoryConnector repo = odysseus.createResourceConnector(resource, false);
 			odysseus.repositoryConfig().setConnector(repo);
 			odysseus.simulationConfig().setFlushInterval(10_000);
+			odysseus.simulationConfig().stopAfterIterions(200_000);
 			odysseus.simulationConfig().setMutatorSelector(MutatorSelector.baseLikelyhood());
-			odysseus.simulationConfig().setStopCriterion(context -> {
-				if (context.getIteration() % 1000 != 0) {
-					return false;
-				}
-				long count = context.countInstancesOf(EcorePackage.Literals.EOBJECT, true);
-				System.out.println("EObjects: " + count);
-				return count >= 200_000;
-			});
-			odysseus.randomGeneratorConfig().setRandomSeed("SoSyM2018");
+			odysseus.randomGeneratorConfig().setRandomSeed("MODELS2017");
 			odysseus.runSimulation();
 			// afterwards, print the model
 			String xmi = FileUtils.readFileToString(tempFile, StandardCharsets.UTF_8);
@@ -121,4 +126,73 @@ public class OdysseusItLandscapeTest {
 			}
 		}
 	}
+
+	@Test
+	@Ignore("this is for manual testing")
+	public void canGenerateDenseModel() {
+		Odysseus odysseus = Odysseus.newInstance();
+		odysseus.moduleConfig().setActiveModule(SocialNetworkModule.class);
+		ResourceSet resourceSet = new ResourceSetImpl();
+		File tempDir = Files.createTempDir();
+		tempDir.deleteOnExit();
+		try {
+			File tempFile = new File(tempDir, "test.xmi");
+			tempFile.deleteOnExit();
+			tempFile.createNewFile();
+			Resource resource = resourceSet.createResource(URI.createFileURI(tempFile.getAbsolutePath()));
+			RepositoryConnector repo = odysseus.createResourceConnector(resource, true);
+			odysseus.repositoryConfig().setConnector(repo);
+			odysseus.simulationConfig().setFlushInterval(10_000);
+			odysseus.simulationConfig().setStopCriterion(context -> {
+				if (context.getIteration() % 1000 != 0) {
+					return false;
+				}
+				long count = context.countInstancesOf(EcorePackage.Literals.EOBJECT, true);
+				System.out.println("EObjects: " + count);
+				return count >= 200_000;
+			});
+			odysseus.simulationConfig().setMutatorSelector(MutatorSelector.baseLikelyhood()
+					//
+					.likelyhood(AddCommentToPostMutator.class, 10)
+					//
+					.likelyhood(AddFriendMutator.class, 10)
+					//
+					.likelyhood(AddReactionToPostMutator.class, 20)
+					//
+					.likelyhood(AddUserMutator.class, 5)
+					//
+					.likelyhood(ChildBirthMutator.class, 1)
+					//
+					.likelyhood(CreateGroupMutator.class, 3)
+					//
+					.likelyhood(CreatePostMutator.class, 14)
+					//
+					.likelyhood(JoinGroupMutator.class, 4)
+					//
+					.likelyhood(MarriageMutator.class, 2)
+					//
+					.likelyhood(SharePostMutator.class, 8)
+					//
+					.likelyhood(UploadMediaFileMutator.class, 5));
+			odysseus.randomGeneratorConfig().setRandomSeed("MODELS2017");
+			odysseus.runSimulation();
+			// afterwards, print the model
+			String xmi = FileUtils.readFileToString(tempFile, StandardCharsets.UTF_8);
+			System.out.println(StringUtils.abbreviate(xmi, 3000));
+			System.out.println("TEMP DIR IS AT: " + tempDir.getAbsolutePath());
+			System.out.println("PRESS ENTER TO EXIT");
+			try (Scanner scanner = new Scanner(System.in)) {
+				scanner.nextLine();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				FileUtils.deleteDirectory(tempDir);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
 }
